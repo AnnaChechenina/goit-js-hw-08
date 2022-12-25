3afe98056457c8760e152466b217814a4a9f1383
AnnaChechenina <annachechenina@gmail.com>
2022-12-25 19:48:15 +0300

    changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,40 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
-
-form.addEventListener(
-  'input',
-  throttle(event => {
-    const objectToSave = { email: email.value, message: message.value };
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
-  }, 500)
-);
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  console.log({ email: email.value, message: message.value });
-  form.reset();
-  localStorage.removeItem(LOCALSTORAGE_KEY);
-});
-
-const load = key => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
 };
 
-const storageData = load(LOCALSTORAGE_KEY);
-if (storageData) {
-  email.value = storageData.email;
-  message.value = storageData.message;
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+
+refs.form.addEventListener('input', throttle(onSaveInput, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  if (formData.email && formData.message) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  } else {
+    alert('Заполните все поля, пожалуйста!');
+  }
+}
+
+function onSaveInput(event) {
+  const userMessage = event.target.value;
+  const userEmail = event.target.name;
+  formData[userEmail] = userMessage;
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+function getFormOutput() {
+  const savedFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (savedFormData) {
+    refs.email.value = savedFormData.email || '';
+    refs.message.value = savedFormData.message || '';
+  }
 }
