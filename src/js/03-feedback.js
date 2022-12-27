ac8263b56f2cda360e5a8bd56499e1b47d346527
AnnaChechenina <annachechenina@gmail.com>
2022-12-27 21:50:40 +0200

    last

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,14 +13,16 @@ refs.form.addEventListener('input', throttle(onSaveInput, 500));
 refs.form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(event) {
+  event.preventDefault();
+
   if (formData.email && formData.message) {
-    event.preventDefault();
-    event.currentTarget.reset();
     console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
     localStorage.removeItem(LOCALSTORAGE_KEY);
   } else {
     alert('Заполните все поля, пожалуйста!');
   }
+  event.currentTarget.reset();
+  localStorage.clear();
 }
 
 function onSaveInput(event) {
@@ -28,14 +30,21 @@ function onSaveInput(event) {
   const userMessage = event.target.value;
   const userEmail = event.target.name;
   formData[userEmail] = userMessage;
-
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 function getFormOutput() {
   const savedFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  try {
+    const dataArr = JSON.parse(savedFormData);
+    if (dataArr.email) {
+      ref.email.value = dataArr.email;
+    }
 
-  if (savedFormData) {
-    refs.email.value = savedFormData.email || '';
-    refs.message.value = savedFormData.message || '';
+    if (dataArr.message) {
+      ref.message.value = dataArr.message;
+    }
+  } catch (error) {
+    error.name;
+    error.message;
   }
 }
