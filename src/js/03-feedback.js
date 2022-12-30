ba1a6b59d77ed6f96f5f396142f50d1897e24fc6
AnnaChechenina <annachechenina@gmail.com>
2022-12-30 20:40:21 +0200

    lastchanges

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,65 @@
 import throttle from 'lodash.throttle';
-
+// ініціалізація елементів форми
 const refs = {
   form: document.querySelector('.feedback-form'),
   email: document.querySelector('input'),
   message: document.querySelector('textarea'),
 };
-
+// Ключ для сховища
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-const formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
-getFormOutput();
+
+// Змінні форми
+let formData = {
+  email: '',
+  message: '',
+};
+// Під час завантаження сторінки перевіряй стан
+// сховища, і якщо там є збережені дані, заповнюй ними
+// поля форми.В іншому випадку поля повинні бути порожніми.
+const stringFormData = localStorage.getItem(LOCALSTORAGE_KEY);
+if (stringFormData) {
+  fillFormOnLoad(stringFormData);
+}
+// відстеження події input і submit з throttle затримка 500ms
 refs.form.addEventListener('input', throttle(onSaveInput, 500));
 refs.form.addEventListener('submit', onSubmitForm);
 
+// Функція події submit
+// заповнення всіх полів обовязкове
+// виведення у консоль об'єкту з полями email,
+//  message та їхніми поточними значеннями.
+// Під час сабміту очищення сховища і полів
+
 function onSubmitForm(event) {
   event.preventDefault();
-
-  if (formData.email && formData.message) {
-    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
-    localStorage.removeItem(LOCALSTORAGE_KEY);
-  } else {
+  if (formData.email === '' || formData.message === '') {
     alert('Заполните все поля, пожалуйста!');
+    return;
   }
   event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
   localStorage.clear();
 }
-
+// Функція події input
+// Відстеж, на формі події input, і щоразу запис
+// у локальне сховище об'єкту з полями email і message,
+// з поточ значен полів .
 function onSaveInput(event) {
-  event.preventDefault();
-  const userMessage = event.target.value;
-  const userEmail = event.target.name;
-  formData[userEmail] = userMessage;
+  if (localStorage.getItem(LOCALSTORAGE_KEY) === null) {
+    formData = {
+      email: '',
+      message: '',
+    };
+  }
+  formData[event.target.name] = event.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
-function getFormOutput() {
-  const savedFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+// обробка помилок
+function fillFormOnLoad(str) {
   try {
-    const dataArr = JSON.parse(savedFormData);
-    if (dataArr.email) {
-      ref.email.value = dataArr.email;
-    }
-
-    if (dataArr.message) {
-      ref.message.value = dataArr.message;
+    formData = JSON.parse(str);
+    for (const key in formData) {
+      refs.form.elements[key].value = formData[key];
     }
   } catch (error) {
     error.name;
